d473d341f7cc954317f77bfa6f849a94f47c452a
Andro Chvertkini <chvertkini.andro@gmail.com>
2021-10-27 14:28:51 +0000

    Added ChangeLog menu

--- static/src/js/support.js
+++ static/src/js/support.js
@@ -16,14 +16,14 @@ odoo.define('asterisk_plus.support', function (require) {
     core.action_registry.add('asterisk_plus.support', Support);
 });
 
-odoo.define('asterisk_plus.addons', function (require) {
+odoo.define('asterisk_plus.change_log', function (require) {
     "use strict";
 
     var AbstractAction = require('web.AbstractAction');
     var core = require('web.core');
 
-    var Addons = AbstractAction.extend({
-        template: 'asterisk_plus.addons',
+    var ChangeLog = AbstractAction.extend({
+        template: 'asterisk_plus.change_log',
 
         init: function (parent, action) {
             this._super.apply(this, arguments);
@@ -31,7 +31,7 @@ odoo.define('asterisk_plus.addons', function (require) {
 
     });
 
-    core.action_registry.add('asterisk_plus.addons', Addons);
+    core.action_registry.add('asterisk_plus.change_log', ChangeLog);
 });
 
 
