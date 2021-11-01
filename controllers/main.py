718a9f0de19039bbcd777ce4ed6de3c5a5239566
Giorgi Tsitskishvili <georgia20000@gmail.com>
2021-11-01 07:07:32 +0000

    controllers as it was in common just changed ip return part previusly code was...

--- controllers/main.py
+++ controllers/main.py
@@ -2,63 +2,158 @@
 import logging
 from odoo import http, SUPERUSER_ID, registry
 from odoo.api import Environment
-from werkzeug.wrappers.response import Response
+from werkzeug.exceptions import BadRequest
+from ..models.settings import debug
 
 logger = logging.getLogger(__name__)
 
-from . common import request_wrapper
 
 class AsteriskPlusController(http.Controller):
 
+    def check_ip(self, db=None):
+        if db:
+            with registry(db).cursor() as cr:
+                env = Environment(cr, SUPERUSER_ID, {})
+                allowed_ips = env[
+                    'asterisk_plus.settings'].sudo().get_param(
+                    'permit_ip_addresses')
+        else:
+            allowed_ips = http.request.env[
+                'asterisk_plus.settings'].sudo().get_param(
+                'permit_ip_addresses')
+        if allowed_ips:
+            remote_ip = http.request.httprequest.remote_addr
+            if remote_ip not in [
+                    k.strip(' ') for k in allowed_ips.split(',')]:
+                return BadRequest(
+                    'Your IP address {} is not allowed!'.format(remote_ip))
+
+    def _get_partner_by_number(self, db, number, country_code):
+        # If db is passed init env for this db
+        dst_partner_info = {'id': None}  # Defaults
+        if db:
+            try:
+                with registry(db).cursor() as cr:
+                    env = Environment(cr, SUPERUSER_ID, {})
+                    dst_partner_info = env[
+                        'res.partner'].sudo().get_partner_by_number(
+                        number, country_code)
+            except Exception:
+                logger.exception('Db init error:')
+                return BadRequest('Db error, check Odoo logs')
+        else:
+            dst_partner_info = http.request.env[
+                'res.partner'].sudo().get_partner_by_number(
+                number, country_code)
+        return dst_partner_info
+
     @http.route('/asterisk_plus/get_caller_name', type='http', auth='none')
-    @request_wrapper
-    def get_caller_name(self, common, dst_partner_info, **kwargs):
-        logger.debug(
-                'CALLER NAME REQUEST FOR NUMBER {} country {}'.format(
-                    common.number, common.country_code))
-        if dst_partner_info.get('id'):
-            return dst_partner_info['name']
-        return Response('', status=200)
+    def get_caller_name(self, **kw):
+        db = kw.get('db')
+        try:
+            checked = self.check_ip(db=db)
+            if checked is not None:
+                return checked
+            number = kw.get('number').replace(' ', '')  # Strip spaces
+            country_code = kw.get('country') or False
+            if not number:
+                return BadRequest('Number not specified in request')
+            debug(
+                http.request, 'get_caller_name',
+                'CALLER NAME REQUEST FOR NUMBER {} country {}'.format(number, country_code))
+            dst_partner_info = self._get_partner_by_number(
+                db, number, country_code)
+            if dst_partner_info['id']:
+                return dst_partner_info['name']
+            return ''
+        except Exception as e:
+            logger.exception('Error:')
+            if 'request not bound to a database' in str(e):
+                return 'db_not_specified'
+            elif 'database' in str(e) and 'does not exist' in str(e):
+                return 'db_not_exists'
+            else:
+                return 'Error'
 
     @http.route('/asterisk_plus/get_partner_manager', auth='public')
-    @request_wrapper
-    def get_partner_manager(self, common, dst_partner_info, **kwargs):
-        if dst_partner_info.get('id'):
-            partner = http.request.env['res.partner'].sudo().browse(
+    def get_partner_manager(self, **kw):
+        db = kw.get('db')
+        try:
+            checked = self.check_ip(db=db)
+            if checked is not None:
+                return checked
+            number = kw.get('number').replace(' ', '')  # Strip spaces
+            country_code = kw.get('country') or False
+            if not number:
+                return BadRequest('Number not specified in request')
+            dst_partner_info = self._get_partner_by_number(
+                db, number, country_code)
+            if dst_partner_info['id']:
+                # Partner found, get manager.
+                partner = http.request.env['res.partner'].sudo().browse(
                     dst_partner_info['id'])
-            if partner.user_id:
-                    # We have sales person set lets check if he has extension.
+                if partner.user_id:
+
+                    # We have sales person set let check if he has extension.
                     if partner.user_id.asterisk_users:
-                        # We have user configured so lets return his channel
-                        originate_channels = [k.name for k in partner.user_id.asterisk_users[0].channels if k.originate_enabled]
+                        # We have user configured so let return his exten
+                        originate_channels = [
+                            k.name for k in partner.user_id.asterisk_users[0].channels
+                            if k.originate_enabled]
                         result = '&'.join(originate_channels)
                         logger.info(
-                            "Returning partner %s manager's exten %s",
+                            "Returning partner %s manager's channel %s",
                             partner.name, result)
-                        return Response(result, status=200)
-            return Response('', status=200)
+                        return result
+            return ''
+        except Exception as e:
+            logger.exception('Error:')
+            if 'request not bound to a database' in str(e):
+                return 'db_not_specified'
+            elif 'database' in str(e) and 'does not exist' in str(e):
+                return 'db_not_exists'
+            else:
+                return 'Error'
 
     @http.route('/asterisk_plus/get_caller_tags', auth='none', type='http')
-    @request_wrapper
-    def get_caller_tags(self, common, dst_partner_info, **kwargs):
-        if dst_partner_info.get('id'):
-            # Partner found, get manager.
-            partner = http.request.env['res.partner'].sudo().browse(
-                dst_partner_info['id'])
-            if partner:
-                return Response(','.join([k.name for k in partner.category_id]), status=200)
-        return Response('', status=200)
+    def get_caller_tags(self, **kw):
+        db = kw.get('db')
+        try:
+            checked = self.check_ip(db=db)
+            if checked is not None:
+                return checked
+            number = kw.get('number').replace(' ', '')  # Strip spaces
+            country_code = kw.get('country') or False
+            if not number:
+                return BadRequest('Number not specified in request')
+            dst_partner_info = self._get_partner_by_number(
+                db, number, country_code)
+            if dst_partner_info['id']:
+                # Partner found, get manager.
+                partner = http.request.env['res.partner'].sudo().browse(
+                    dst_partner_info['id'])
+                if partner:
+                    return ','.join([k.name for k in partner.category_id])
+            return ''
+        except Exception as e:
+            logger.exception('Error:')
+            if 'request not bound to a database' in str(e):
+                return 'db_not_specified'
+            elif 'database' in str(e) and 'does not exist' in str(e):
+                return 'db_not_exists'
+            else:
+                return 'Error'
 
-    @http.route('/asterisk_plus/asterisk_ping', type='http', auth='none')
+    @http.route('/asterisk_plus/ping', type='http', auth='none')
     def asterisk_ping(self):
         with registry('odoopbx_14').cursor() as cr:
             env = Environment(cr, SUPERUSER_ID, {})
             try:
-                res = env['asterisk_plus.server'].browse(1).salt_job(
+                res = env['asterisk_plus.server'].browse(1).local_job(
                     fun='test.ping', sync=True)
                 return http.Response('{}'.format(res))
             except Exception as e:
-                logger.exception('Error')
+                logger.exception('Error:')
                 return '{}'.format(e)
 
     @http.route('/asterisk_plus/asterisk_ping', type='http', auth='none')
@@ -70,7 +165,7 @@ class AsteriskPlusController(http.Controller):
                     {'Action': 'Ping'}, sync=True)
                 return http.Response('{}'.format(res))
             except Exception as e:
-                logger.exception('Error')
+                logger.exception('Error:')
                 return '{}'.format(e)
 
     @http.route('/asterisk_plus/signup', auth='user')
@@ -89,4 +184,3 @@ class AsteriskPlusController(http.Controller):
         mail.send()
         return http.request.render('asterisk_plus.email_sent',
                                    qcontext={'email': email})
-
