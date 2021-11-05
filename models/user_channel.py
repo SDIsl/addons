d05c425e862775e98581d68cd6faca3ec3907606
Andro Chvertkini <chvertkini.andro@gmail.com>
2021-11-05 11:04:28 +0400

    Minor originate settings changes [skip-ci]

--- models/user_channel.py
+++ models/user_channel.py
@@ -50,13 +50,10 @@ class UserChannel(models.Model):
     sequence = fields.Integer(default=100, index=True)
     #: Dial this channel when click to call is used.
     originate_enabled = fields.Boolean(default=True, string="Originate")
-    # Store originate_context here.
-    context = fields.Char()
     #: O
     originate_context = fields.Char(
         default=lambda self: self._get_default_context(),
-        string='Context',
-        compute='_get_originate_context', inverse='_set_originate_context')
+        string='Context')
     auto_answer_header = fields.Selection(AUTO_ANSWER_HEADERS)
 
     _sql_constraints = [
@@ -92,13 +89,5 @@ class UserChannel(models.Model):
                 raise ValidationError()
 
     def _get_default_context(self):
-        return self.env['asterisk_plus.settings'].get_param(
+        return self.env['asterisk_plus.settings'].sudo().get_param(
             'originate_context', 'from-internal')
-
-    def _get_originate_context(self):
-        for rec in self:
-            rec.originate_context = rec.context
-
-    def _set_originate_context(self):
-        for rec in self:
-            rec.context = rec.originate_context
