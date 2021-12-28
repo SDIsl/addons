de7e9e56685a09c8f5c6984dd58e5b7bf478a090
Nicolae Postica <nicolae.postica2@gmail.com>
2021-12-29 01:44:02 +0200

    fix originate.js [skip-ci]

--- static/src/js/originate.js
+++ static/src/js/originate.js
@@ -8,16 +8,19 @@ odoo.define('asterisk_plus.originate_widget', function (require) {
 
     _renderReadonly: function () {
       this._super();
-      var self = this
-      this.$el.append('&nbsp;<button type="button" class="originate-call-lg-btn originate_call_button btn btn-lg btn-primary fa fa-phone" \
-                      aria-label="Call" title="Call"></button>')
-      this.$el.find('.originate_call_button').click(function(){
-        return self._rpc({
+      var self = this;
+      if (self.value) {
+        this.$el.prop("onclick", null).off("click");
+        this.$el.append('&nbsp;<button type="button" class="originate-call-lg-btn originate_call_button btn btn-lg btn-primary fa fa-phone" \
+                        aria-label="Call" title="Call"></button>')
+        this.$el.find('.originate_call_button').click(function () {
+          const el = self._rpc({
             'model': 'asterisk_plus.server',
             'method': 'originate_call',
             'args': [self.value, self.model, self.res_id]
+          })
         })
-      })
+      }
     },
   })
 
@@ -25,16 +28,19 @@ odoo.define('asterisk_plus.originate_widget', function (require) {
 
     _renderReadonly: function () {
       this._super();
-      var self = this
-      this.$el.append('&nbsp;<button type="button" class="originate-call-lg-btn originate_extension_button btn btn-lg btn-primary fa fa-phone" \
-                      aria-label="Call" title="Call"></button>')
-      this.$el.find('.originate_extension_button').click(function(){
-        return self._rpc({
+      var self = this;
+      if (self.value) {
+        this.$el.prop("onclick", null).off("click");
+        this.$el.append('&nbsp;<button type="button" class="originate-call-lg-btn originate_extension_button btn btn-lg btn-primary fa fa-phone" \
+                        aria-label="Call" title="Call"></button>')
+        this.$el.find('.originate_extension_button').click(function () {
+          const el = self._rpc({
             'model': 'res.partner',
             'method': 'originate_call',
             'args': [self.value, self.model, self.res_id]
+          })
         })
-      })
+      }
     },
   })
 
