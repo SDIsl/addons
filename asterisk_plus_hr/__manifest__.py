1d9cfa66bfda18201459636936171352be847f2f
Andro Chvertkini <chvertkini.andro@gmail.com>
2022-01-19 15:20:38 +0400

    added asterisk_plus_hr 11.0

---
+++ asterisk_plus_hr/__manifest__.py
@@ -0,0 +1,25 @@
+# ©️ OdooPBX by Odooist, Odoo Proprietary License v1.0, 2021
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Asterisk Plus HR',
+    'version': '1.0',
+    'author': 'Odooist',
+    'price': 0,
+    'currency': 'EUR',
+    'maintainer': 'Odooist',
+    'support': 'odooist@gmail.com',
+    'license': 'LGPL-3',
+    'category': 'Phone',
+    'summary': 'Asterisk Plus HR integration',
+    'description': "",
+    'depends': ['hr', 'asterisk_plus'],
+    'data': [
+        'security/server.xml',
+        'views/hr_views.xml',
+    ],
+    'demo': [],
+    "qweb": ['static/src/xml/*.xml'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ asterisk_plus_hr/models/call.py
@@ -0,0 +1,10 @@
+# ©️ OdooPBX by Odooist, Odoo Proprietary License v1.0, 2021
+from odoo import models, fields
+
+
+class HrCall(models.Model):
+    _inherit = 'asterisk_plus.call'
+
+    ref = fields.Reference(selection_add=[
+        ('hr.employee', 'Employee'),
+    ])
