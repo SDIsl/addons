1131c09e527464c3132044d2f40e27409e6ec3f5
Andro Chvertkini <chvertkini.andro@gmail.com>
2022-01-25 19:07:57 +0400

    added asterisk_plus_crm 15.0

---
+++ asterisk_plus_crm/__manifest__.py
@@ -0,0 +1,29 @@
+# ©️ OdooPBX by Odooist, Odoo Proprietary License v1.0, 2021
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Asterisk Plus CRM',
+    'version': '1.0',
+    'author': 'Odooist',
+    'price': 0,
+    'currency': 'EUR',
+    'maintainer': 'Odooist',
+    'support': 'odooist@gmail.com',
+    'license': 'LGPL-3',
+    'category': 'Phone',
+    'summary': 'Asterisk Plus CRM integration',
+    'description': "",
+    'depends': ['crm', 'asterisk_plus'],
+    'data': [
+        'security/server.xml',
+        'views/crm_lead.xml',
+        'views/call.xml',
+        'views/settings.xml',
+        
+    ],
+    'demo': [],
+    "qweb": ['static/src/xml/*.xml'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'images': ['static/description/logo.png'],
+}
---
+++ asterisk_plus_crm/models/call.py
@@ -0,0 +1,72 @@
+# ©️ OdooPBX by Odooist, Odoo Proprietary License v1.0, 2021
+import logging
+from odoo import models, fields, api, _
+from odoo.addons.asterisk_plus.models.settings import debug
+
+logger = logging.getLogger(__name__)
+
+
+class CrmCall(models.Model):
+    _inherit = 'asterisk_plus.call'
+
+    ref = fields.Reference(selection_add=[('crm.lead', 'Leads')])
+
+    def update_reference(self):
+        res = super(CrmCall, self).update_reference()
+        if not res:
+            lead = None
+            # No reference was set, so we have a change to set it to a lead
+            debug(self, 'DIRECTION: {}'.format(self.direction))
+            if self.direction == 'in':                
+                lead = self.env['crm.lead'].get_lead_by_number(self.calling_number)
+            else:
+                lead = self.env['crm.lead'].get_lead_by_number(self.called_number)
+            debug(self, 'LEAD: {}'.format(lead))
+            if lead:
+                self.ref = lead
+                return True
+
+    @api.constrains('is_active', 'direction')
+    def auto_create_lead(self):
+        auto_create_leads = self.env['asterisk_plus.settings'].get_param(
+            'auto_create_leads_from_calls')
+        only_missed = self.env[
+            'asterisk_plus.settings'].get_param(
+            'auto_create_leads_missed_calls_only')
+        default_sales_person = self.env[
+            'asterisk_plus.settings'].get_param(
+            'auto_create_leads_sales_person')
+        for rec in self:            
+            if not self.direction == 'in' or rec.ref:
+                # We only do it for incoming calls without reference.
+                continue
+            if not rec.is_active:
+                # Call end
+                if auto_create_leads and only_missed and rec.status != 'answered':
+                    debug(self, 'CREATE LEAD FROM MISSED CALL')
+                    lead = self.env['crm.lead'].create({
+                        'name': rec.calling_name,
+                        'type': 'lead',
+                        'user_id': rec.called_user.id or default_sales_person.id,
+                        'partner_id': rec.partner.id,
+                        'phone': rec.calling_number,
+                    })
+                    rec.ref = lead
+            else:
+                # Call start
+                lead = self.env['crm.lead'].get_lead_by_number(self.calling_number)
+                if not lead:
+                    if auto_create_leads and not only_missed:
+                        debug(self, 'CREATE LEAD FROM CALL START')
+                        lead = self.env['crm.lead'].create({
+                            'name': rec.calling_name,
+                            'type': 'lead',
+                            'user_id': rec.called_user.id or default_sales_person.id,
+                            'partner_id': rec.partner.id,
+                            'phone': rec.calling_number,
+                        })
+                        rec.ref = lead
+                else:
+                    # Lead found
+                    rec.ref = lead
+
