02b096f9125979eb1a6b1e2e1535457bfd33848f
Andro Chvertkini <chvertkini.andro@gmail.com>
2022-01-25 13:25:09 +0400

    added asterisk_plus_sale 15.0

---
+++ asterisk_plus_sale/models/call.py
@@ -0,0 +1,20 @@
+# ©️ OdooPBX by Odooist, Odoo Proprietary License v1.0, 2021
+from odoo import models, fields
+
+
+class SaleCall(models.Model):
+    _name = 'asterisk_plus.call'
+    _inherit = 'asterisk_plus.call'
+
+    ref = fields.Reference(selection_add=[
+        ('sale.order', 'Sales')])
+
+    def update_reference(self):
+        res = super(SaleCall, self).update_reference()
+        if not res:
+            if self.partner:
+                sale_order = self.env['sale.order'].search([
+                    ('partner_id', '=', self.partner.id)], limit=1)
+                if sale_order:
+                    self.ref = sale_order
+                    return True
